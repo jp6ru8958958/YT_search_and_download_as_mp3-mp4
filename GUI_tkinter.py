fb7f5de13d2107683c294abf92a10172b31b5342
jp6ru8958958 <jp6ru8958958@gmail.com>
2018-12-03 23:31:06 +0800

    hi,macs1207

--- GUI_tkinter.py
+++ GUI_tkinter.py
@@ -9,30 +9,45 @@ main_window.geometry('350x200')
 
 def button1_click():
     key_word = main_entry.get()
-    main_label["text"]="按鈕1"
+    url = "https://www.youtube.com/results?sp=CAM%253D&search_query=" + key_word
+    show_label1["text"]='影片名稱:'+key_word
+    show_label2["text"]='影片觀看次數:'+key_word
 
 def button2_click():
     key_word = main_entry.get()
-    main_label["text"]="按鈕2"
+    show_label1["text"]=key_word
+    show_label2["text"]='影片觀看次數:'+key_word
+
+def button3_click():
+    key_word = main_entry.get()
+    show_label1["text"]=key_word
 
 #TEXT ENTRY=============================================================================================================
 
 entry_read=StringVar()
-main_entry = tk.Entry(main_window,width=20,textvariable=entry_read)
+main_entry = tk.Entry(main_window,width=25,bd=3,textvariable=entry_read)
 
 #BUTTON=================================================================================================================
 
-button_one = tk.Button(main_window,text="按鈕1",width=10,height=2,command=button1_click)
-button_two = tk.Button(main_window,text="按鈕2",width=10,height=2,command=button2_click)
+button_one = tk.Button(main_window,text="MP3下載",width=10,height=2,command=button1_click)
+button_two = tk.Button(main_window,text="MP4下載",width=10,height=2,command=button2_click)
+button_three = tk.Button(main_window,text="NEXT",width=5,height=2,command=button3_click)
 
 #LABEL==================================================================================================================
 
+
+show_label1=Label(main_window)
+show_label2=Label(main_window)
 main_label=Label(main_window)
+main_label["text"] = "輸入關鍵字"
 
 #PACK===================================================================
 
 main_entry.place(x=100, y=20, anchor='nw')
-main_label.place(x=100, y=40, anchor='nw')
-button_one.place(x=50, y=150, anchor='nw')
+main_label.place(x=30,y=20,anchor='nw')
+show_label1.place(x=30, y=60, anchor='nw')
+show_label2.place(x=30, y=90, anchor='nw')
+button_one.place(x=100, y=150, anchor='nw')
 button_two.place(x=200, y=150, anchor='nw')
+button_three.place(x=300, y=25, anchor='nw')
 main_window.mainloop()
